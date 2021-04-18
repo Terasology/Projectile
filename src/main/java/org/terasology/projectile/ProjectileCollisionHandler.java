2dcda89f5426e5d6ec65cdf9cfac21eda504388a
Tobias Nett <skaldarnar@googlemail.com>
2021-04-18 19:02:19 +0200

    refactor: adjust for restructured Health v2.0.0-SNAPSHOT (#17)

--- src/main/java/org/terasology/projectile/ProjectileCollisionHandler.java
+++ src/main/java/org/terasology/projectile/ProjectileCollisionHandler.java
@@ -21,7 +21,7 @@ import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.health.event.DoDamageEvent;
+import org.terasology.module.health.events.DoDamageEvent;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class ProjectileCollisionHandler extends BaseComponentSystem {
