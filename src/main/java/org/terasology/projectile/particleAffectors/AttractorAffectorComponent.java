b31cff271de7f38be5083cb64e68f58f31c3fb34
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:17 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/projectile/particleAffectors/AttractorAffectorComponent.java
+++ src/main/java/org/terasology/projectile/particleAffectors/AttractorAffectorComponent.java
@@ -1,29 +1,16 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.projectile.particleAffectors;
 
 import org.joml.Vector3f;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.logic.location.LocationComponent;
 import org.terasology.engine.network.Replicate;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class AttractorAffectorComponent implements Component {
+public class AttractorAffectorComponent implements Component<AttractorAffectorComponent> {
 
     @Replicate
     public Map<Vector3f, Float> attractors;
@@ -35,4 +22,13 @@ public class AttractorAffectorComponent implements Component {
         this.attractors = new HashMap<>();
     }
 
+    @Override
+    public void copy(AttractorAffectorComponent other) {
+        this.attractors.clear();
+        for (Map.Entry<Vector3f, Float> entry : other.attractors.entrySet()) {
+            this.attractors.put(new Vector3f(entry.getKey()), entry.getValue());
+        }
+
+        this.origin.copy(other.origin);
+    }
 }
