8a1b9436990952594667c9284e2cc6387f2d403a
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 17:34:46 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#19)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/projectile/ProjectileActionComponent.java
+++ src/main/java/org/terasology/projectile/ProjectileActionComponent.java
@@ -1,26 +1,13 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.projectile;
 
 import org.joml.Vector3f;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.entitySystem.prefab.Prefab;
 import org.terasology.engine.logic.health.EngineDamageTypes;
+import org.terasology.gestalt.entitysystem.component.Component;
 
-public class ProjectileActionComponent implements Component {
+public class ProjectileActionComponent implements Component<ProjectileActionComponent> {
     /**
      * scaling the projectile icon
      */
@@ -70,4 +57,18 @@ public class ProjectileActionComponent implements Component {
     public float projectilesPerSecond = 1.0f;
 
     public Prefab damageType = EngineDamageTypes.PHYSICAL.get();
+
+    @Override
+    public void copyFrom(ProjectileActionComponent other) {
+        this.iconScale = other.iconScale;
+        this.initialOrientation = new Vector3f(other.initialOrientation);
+        this.reusable = other.reusable;
+        this.affectedByGravity = other.affectedByGravity;
+        this.frictionCoefficient = other.frictionCoefficient;
+        this.initialVelocity = other.initialVelocity;
+        this.maxDistance = other.maxDistance;
+        this.damageAmount = other.damageAmount;
+        this.projectilesPerSecond = other.projectilesPerSecond;
+        this.damageType = other.damageType;
+    }
 }
--- src/main/java/org/terasology/projectile/ProjectileMotionComponent.java
+++ src/main/java/org/terasology/projectile/ProjectileMotionComponent.java
@@ -1,27 +1,14 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.projectile;
 
 import org.joml.Vector3f;
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * Component attached to projectiles which are in state of motion ,i.e have been fired.
  */
-public class ProjectileMotionComponent implements Component {
+public class ProjectileMotionComponent implements Component<ProjectileMotionComponent> {
 
     /**
      * The current velocity of projectile.
@@ -38,4 +25,10 @@ public class ProjectileMotionComponent implements Component {
      */
     public float distanceTravelled = 0;
 
+    @Override
+    public void copyFrom(ProjectileMotionComponent other) {
+        this.currentVelocity = new Vector3f(other.currentVelocity);
+        this.direction = new Vector3f(other.direction);
+        this.distanceTravelled = other.distanceTravelled;
+    }
 }
